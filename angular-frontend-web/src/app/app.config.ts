0486aeeb4b8bcdf1a421d3350da7ddb6e48c7f9f
nuntichaSon <nunticha.son@gmail.com>
2025-11-13 16:30:09 +0700

    Add angular-frontend-web project as normal folder

---
+++ angular-frontend-web/src/app/app.config.ts
@@ -0,0 +1,9 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './app-routing.module.routes';
+
+
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+};
